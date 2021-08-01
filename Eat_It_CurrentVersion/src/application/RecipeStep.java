5fcebc4ad012177e79b4b3907efc7dd3d9aee232
sergiogutierrez2 <58555535+sergiogutierrez2@users.noreply.github.com>
2021-08-01 05:40:10 -0700

    Added Javadoc

--- Eat_It_CurrentVersion/src/application/RecipeStep.java
+++ Eat_It_CurrentVersion/src/application/RecipeStep.java
@@ -1,7 +1,7 @@
 package application;
 
 /**
- * TODO: Write a description of the class here.
+ * Java Class that manages the steps of each of the recipes.
  * 
  * @author Eat_It(Summer 2021 Team)
  */
@@ -11,6 +11,13 @@ public class RecipeStep
 	private String step_num;
 	private String step_desc;
 	
+	/**
+	 * This is the constructor for the RecipeStep class that accepts
+	 * a recipe number, step number, and step description.
+	 * @param recipe_num Number of the recipe.
+	 * @param step_num Number of the step.
+	 * @param step_desc Step description.
+	 */
 	public RecipeStep(String recipe_num, String step_num, String step_desc) 
 	{
 		this.recipe_num = recipe_num;
@@ -18,30 +25,61 @@ public class RecipeStep
 		this.step_desc = step_desc;
 	}
 	
+	/**
+	 * This method returns the number of the recipe.
+	 * @return The number of the recipe.
+	 */
 	public String getRecipe_num() {
 		return recipe_num;
 	}
 	
+	/**
+	 * This method is used to set the recipe's number
+	 * with the given parameter.
+	 * @param recipe_num The number of the recipe.
+	 */
 	public void setRecipe_num(String recipe_num) {
 		this.recipe_num = recipe_num;
 	}
 	
+	/**
+	 * This method returns the number of the step.
+	 * @return The number of the step.
+	 */
 	public String getStep_num() {
 		return step_num;
 	}
 	
+	/**
+	 * This method is used to set the recipe's step number
+	 * with the given parameter.
+	 * @param step_num The number of the step of the recipe.
+	 */
 	public void setStep_num(String step_num) {
 		this.step_num = step_num;
 	}
 	
+	/**
+	 * This method returns the description of the step.
+	 * @return The description of the step.
+	 */
 	public String getStep_desc() {
 		return step_desc;
 	}
 	
+	/**
+	 * This method is used to set the recipe's step description
+	 * with the given parameter.
+	 * @param step_num The number of the step of the recipe.
+	 */
 	public void setStep_desc(String step_desc) {
 		this.step_desc = step_desc;
 	}
 	
+	/**
+	 * Returns a string representation of the object.
+	 * @return A string representation of the object.
+	 */
 	@Override
 	public String toString()
 	{
